58e5a6aa2a5b26d18dd0cb643ccdf5f9d9014701
Kim Seungchan <seungchan1219@gmail.com>
2025-02-25 15:42:23 +0900

    #070: Capture Attack and Defense attributes

--- Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -2,3 +2,41 @@
 
 
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
+#include "AbilitySystem/WarriorAttributeSet.h"
+
+struct FWarriorDamageCapture
+{
+    DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
+    DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower);
+
+    FWarriorDamageCapture()
+    {
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false)
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false)
+    }
+};
+
+static const FWarriorDamageCapture& GetWarriorDamageCapture()
+{
+    static FWarriorDamageCapture WarriorDamageCapture;
+    return WarriorDamageCapture;
+}
+
+UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
+{
+    /* slow way of capture
+    auto* AttackPowerProperty = FindFieldChecked<FProperty>(UWarriorAttributeSet::StaticClass(),
+        GET_MEMBER_NAME_CHECKED(UWarriorAttributeSet, AttackPower));
+
+    FGameplayEffectAttributeCaptureDefinition AttackPowerDefinition(
+        AttackPowerProperty,
+        EGameplayEffectAttributeCaptureSource::Source,
+        false
+    );
+
+    RelevantAttributesToCapture.Add(AttackPowerDefinition);
+    */
+
+    RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
+    RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
