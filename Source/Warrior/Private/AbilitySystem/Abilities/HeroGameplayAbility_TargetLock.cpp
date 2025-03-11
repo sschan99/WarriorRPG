5f550f929f4a14c5a69d61583c80a98117563f24
Kim Seungchan <seungchan1219@gmail.com>
2025-03-11 09:16:01 +0900

    #160: Get all available targets

--- Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -3,9 +3,15 @@
 
 #include "Abilities/HeroGameplayAbility_TargetLock.h"
 
+#include "WarriorDebugHelper.h"
+#include "WarriorHeroCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
     const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
+    TryLockOnTarget();
+    
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
@@ -13,4 +19,46 @@ void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandl
     const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
     Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+    GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+    TArray<FHitResult> BoxTraceHits;
+    const AWarriorHeroCharacter* HeroCharacter = GetHeroCharacterFromActorInfo();
+    const FVector Start = HeroCharacter->GetActorLocation();
+    const FVector ForwardVector = HeroCharacter->GetActorForwardVector();
+
+    TArray<AActor*> ActorsToIgnore;
+    ActorsToIgnore.Add(GetHeroCharacterFromActorInfo());
+    
+    UKismetSystemLibrary::BoxTraceMultiForObjects(
+        HeroCharacter,
+        Start,
+        Start + ForwardVector * BoxTraceDistance,
+        TraceBoxSize / 2.f,
+        ForwardVector.ToOrientationRotator(),
+        BoxTraceChannel,
+        false,
+        ActorsToIgnore,
+        bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+        BoxTraceHits,
+        true
+        );
+    
+    for (const FHitResult& TraceHit : BoxTraceHits)
+    {
+        if (AActor* HitActor = TraceHit.GetActor())
+        {
+            if (HitActor != HeroCharacter)
+            {
+                AvailableActorsToLock.AddUnique(HitActor);
+                Debug::Print(HitActor->GetActorNameOrLabel());
+            }
+        }
+    }
 }
