51f68b83d88024a706a7bba88d35c4ed38d31406
Kim Seungchan <seungchan1219@gmail.com>
2025-03-11 14:10:26 +0900

    #164: Add an ability task to tick for target lock ability

---
+++ Source/Warrior/Private/AbilitySystem/AbilityTasks/AbilityTask_ExecuteTaskOnTick.cpp
@@ -0,0 +1,28 @@
+﻿// Valerii Diachenko All Rights Reserved.
+
+
+#include "AbilityTasks/AbilityTask_ExecuteTaskOnTick.h"
+
+UAbilityTask_ExecuteTaskOnTick::UAbilityTask_ExecuteTaskOnTick()
+{
+    bTickingTask = true;
+}
+
+UAbilityTask_ExecuteTaskOnTick* UAbilityTask_ExecuteTaskOnTick::ExecuteTaskOnTick(UGameplayAbility* OwningAbility)
+{
+    return NewAbilityTask<UAbilityTask_ExecuteTaskOnTick>(OwningAbility);
+}
+
+void UAbilityTask_ExecuteTaskOnTick::TickTask(float DeltaTime)
+{
+    Super::TickTask(DeltaTime);
+
+    if (ShouldBroadcastAbilityTaskDelegates())
+    {
+        OnAbilityTaskTick.Broadcast(DeltaTime);
+    }
+    else
+    {
+        EndTask();
+    }
+}
