9cebc8e8ecf8598694f8f7936302f222d85dd3aa
Kim Seungchan <seungchan1219@gmail.com>
2025-05-06 17:21:15 +0900

    #241: On ability icon updated delegate

--- Source/Warrior/Public/Components/UI/HeroUIComponent.h
+++ Source/Warrior/Public/Components/UI/HeroUIComponent.h
@@ -3,10 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "PawnUIComponent.h"
 #include "HeroUIComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class WARRIOR_API UHeroUIComponent : public UPawnUIComponent
@@ -20,4 +23,7 @@ public:
 
     UPROPERTY(BlueprintCallable, BlueprintAssignable)
     FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
+
+    UPROPERTY(BlueprintCallable, BlueprintAssignable)
+    FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
 };
