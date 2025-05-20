8f330d3f04165ebf0d91e6f85a5592a86addd42e
Kim Seungchan <seungchan1219@gmail.com>
2025-05-20 12:11:04 +0900

    #256: Add pickup base class

---
+++ Source/Warrior/Public/Items/PickUps/WarriorStoneBase.h
@@ -0,0 +1,13 @@
+﻿// Valerii Diachenko All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarriorPickUpBase.h"
+#include "WarriorStoneBase.generated.h"
+
+UCLASS()
+class WARRIOR_API AWarriorStoneBase : public AWarriorPickUpBase
+{
+    GENERATED_BODY()
+};
