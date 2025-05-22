98118d25f7c163e5748e6fae9e0a196fd726d57c
Kim Seungchan <seungchan1219@gmail.com>
2025-05-22 12:23:23 +0900

    #258: Start implement pickup ability

---
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.cpp
@@ -0,0 +1,14 @@
+﻿// Valerii Diachenko All Rights Reserved.
+
+
+#include "Abilities/HeroGameplayAbility_PickUpStones.h"
+
+void UHeroGameplayAbility_PickUpStones::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UHeroGameplayAbility_PickUpStones::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
