5916aac7d4acb9c5a69353b2c8ddcc9d042fde80
Kim Seungchan <seungchan1219@gmail.com>
2025-06-02 11:47:54 +0900

    #274: Add draft survial game mode class

---
+++ Source/Warrior/Private/Gamemodes/WarriorSurvialGameMode.cpp
@@ -0,0 +1,26 @@
+﻿// Valerii Diachenko All Rights Reserved.
+
+
+#include "WarriorSurvialGameMode.h"
+
+void AWarriorSurvialGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AWarriorSurvialGameMode::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void AWarriorSurvialGameMode::SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState InState)
+{
+    if (CurrentSurvialGameModeState == InState)
+    {
+        return;
+    }
+    
+    CurrentSurvialGameModeState = InState;
+    
+    OnSurvialGameModeStateChanged.Broadcast(CurrentSurvialGameModeState);
+}
---
+++ Source/Warrior/Public/Gamemodes/WarriorSurvialGameMode.h
@@ -0,0 +1,45 @@
+﻿// Valerii Diachenko All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarriorBaseGameMode.h"
+#include "WarriorSurvialGameMode.generated.h"
+
+UENUM(BlueprintType)
+enum class EWarriorSurvialGameModeState : uint8
+{
+    WaitSpawnNewWave,
+    SpawningNewWave,
+    InProgress,
+    WaveCompleted,
+    AllWavesDone,
+    PlayerDied
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvialGameModeStateChangedDelegate, EWarriorSurvialGameModeState, CurrentState);
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorSurvialGameMode : public AWarriorBaseGameMode
+{
+    GENERATED_BODY()
+
+protected:
+
+    virtual void BeginPlay() override;
+
+    virtual void Tick(float DeltaTime) override;
+
+private:
+
+    void SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState InState);
+
+    UPROPERTY()
+    EWarriorSurvialGameModeState CurrentSurvialGameModeState;
+
+    UPROPERTY(BlueprintAssignable, BlueprintCallable)
+    FOnSurvialGameModeStateChangedDelegate OnSurvialGameModeStateChanged;
+};
